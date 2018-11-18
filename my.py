c9682a1af4442dee2eb006fa21f279dfd704fc04
Gayath <gayath.chandira94@gmail.com>
2018-11-18 22:22:17 +0530

    second

--- my.py
+++ my.py
@@ -3,24 +3,27 @@ import sys
 import time
 import threading
 
-host, port = 'localhost',801
+host, port = 'localhost',8080
 file_route = 'main'
 soc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
 def start_server():
+
     try:
-        print("Starting web server on {host}:{port}.....".format(host=host, port=port))
+        print("Starting server on {host}:{port}.....".format(host=host, port=port))
         soc.bind((host,port))            # binding socket with network interface and port
         print("Server started on port {port}.".format(port=port))
+
     except Exception as e:
+        print(e)            # if port is not available
         print("Error: Could not bind to port {port}".format(port=port))
-        # shutdown()
-        print(e)
         sys.exit(1)       # exit from the program
+        shutdown()
     connect()
 
 def connect():
+
     while True:
         soc.listen(4)     # listening for connctions from client , 4 is number of invalid connections before terminate
         client_connection,client_address = soc.accept()
@@ -40,20 +43,16 @@ def controlling(connection):
             print("Request timeout {e}").format(e=e)
             break
         print(str_data)
-
         fileName = str_data.split(' ')[1].split('?')[0]
-        try:
 
+        try:
 
             if (fileName == '/'):
                 fileName = '/index.html'
             file_path = file_route + fileName
-
             file = open(file_path,'rb')    # read bytes
             response_data = file.read()
             file.close()
-
-
 
             content_type =''
             if fileName.endswith('.html'):
@@ -64,36 +63,37 @@ def controlling(connection):
                 content_type += 'Content-Type: image/png\n'
             elif fileName.endswith('.css'):
                 content_type += 'Content-Type: text/css\n'
+            elif fileName.endswith('.pdf'):
+                content_type += 'Content-Type: application\pdf\n'
+            elif fileName.endswith('.js'):
+                content_type += 'Content-Type: application\javascript\n'
+            elif fileName.endswith('.mp4'):
+                content_type += 'Content-Type: video\mp4\n'
+
+            if "." not in fileName:
+                raise IndexError
+
             header = "HTTP/1.1 200 OK\n"
-            # if type == 'htm' or type == 'html':
-            #     header += 'Content-Type: text/html\n'
-            # elif type == 'jpg' or type == 'jpeg':
-            #     header += 'Content-Type: image/jpeg\n'
-            # elif type == 'png':
-            #     header += 'Content-Type: image/png\n'
-            # elif type == 'css':
-            #     header += 'Content-Type: text/css\n'
 
             header += content_type
             header += 'Connection Close \n\n'
             print(header)
-        except IndexError as e:
-            print(e)
-            header = makeHeader(400,'')
-            response_data = b"<h1>malformed syntax<h1>"
 
-        except UnboundLocalError as e:
+        except IndexError as e:    # if user not defined file type correctly
             print(e)
-            #header = makeHeader(400,'')
-            header = 'HTTP/1.1 400 Bad Request\n'
-            response_data = b"Malformed requested"
-        
+            header = 'HTTP/1.1 400 Bad Request\n\n'
+            response_data = b"<h1>malformed syntax</h1><p>No file type defined!</p>"
+
 
         except FileNotFoundError as e :
             print(e)
-            #header = makeHeader(404,'')
-            header = 'HTTP/1.1 404 Not Found\n'
-            response_data =b"<h1>File not found<h1>"
+            header = 'HTTP/1.1 404 Not Found\n\n'
+            fileName = '/404error.html'
+            file_path = file_route + fileName
+            print(file_path)
+            file = open(file_path,'rb')
+            response_data = file.read()
+            file.close()
 
 
         response = header.encode()
@@ -102,26 +102,7 @@ def controlling(connection):
         connection.close()
         break
 
-# def makeHeader(code, type):
-#     print(type)
-#     header =''
-#     if code == 200:
-#         header +="HTTP/1.1 200 OK\n"
-#     elif code == 404:
-#         header += 'HTTP/1.1 404 Not Found\n'
-#     elif code == 400:
-#         header += 'HTTP/1.1 400 Bad Request\n'
-#
-#     if type == 'jpg' or type == 'jpeg':
-#         header += 'Content-Type: image/jpeg\n'
-#     elif type == 'htm' or type == 'html':
-#         header += 'Content-Type: text/html\n'
-#     elif type == 'png':
-#         header += 'Content-Type: image/png\n'
-#     elif type == 'css':
-#         header += 'Content-Type: text/css\n'
-#
-#     header += 'Connection: close\n\n'
-#     return header
+def shutdown():
+    soc.shutdown(socket.SHUT_RDWR) # shut down the read and write
 
 start_server()
